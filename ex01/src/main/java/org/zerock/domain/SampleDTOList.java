316bb264e39a90fea3a7c04d7456199c842aca23
kimminhun <asdfg6469597@gmail.com>
2021-03-16 23:50:43 +0900

    msg

---
+++ ex01/src/main/java/org/zerock/domain/SampleDTOList.java
@@ -0,0 +1,17 @@
+package org.zerock.domain;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class SampleDTOList {
+	private List<SampleDTO> list;
+	
+	public SampleDTOList() {
+		list=new ArrayList<>();
+	}
+
+
+}
